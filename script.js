1d71bb031c2c5ddab6833b0b0b00a95bc9dd84bd
Etterlivet <78481603+Etterlivet@users.noreply.github.com>
2024-10-01 16:06:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 function addRecommendation() {
-  // Get the message of the new recommendation
-  let recommendation = document.getElementById("new_recommendation");
-  // If the user has left a recommendation, display a pop-up
-  if (recommendation.value != null && recommendation.value.trim() != "") {
-    console.log("New recommendation added");
-    //Call showPopup here
+    const newRecommendationText = document.getElementById("new_recommendation").value;
+    if (newRecommendationText) {
+        const recommendationsContainer = document.getElementById("all_recommendations");
 
-    // Create a new 'recommendation' element and set it's value to the user's message
-    var element = document.createElement("div");
-    element.setAttribute("class","recommendation");
-    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
-    // Add this element to the end of the list of recommendations
-    document.getElementById("all_recommendations").appendChild(element); 
-    
-    // Reset the value of the textarea
-    recommendation.value = "";
-  }
+        // Create a new recommendation element
+        const newRecommendation = document.createElement("div");
+        newRecommendation.classList.add("recommendation");
+        newRecommendation.innerHTML = `<span>&#8220;</span>${newRecommendationText}<span>&#8221;</span>`;
+        
+        // Add the new recommendation to the container
+        recommendationsContainer.appendChild(newRecommendation);
+        
+        // Clear the textarea
+        document.getElementById("new_recommendation").value = "";
+
+        // Show confirmation popup
+        showPopup(true);
+    }
 }
 
 function showPopup(bool) {
